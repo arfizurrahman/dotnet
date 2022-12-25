f59dd8b348781502a2db3a55c7d5609dd82310d9
Mohammad Arfizur Rahman <arahman@numberskills.se>
2022-12-25 20:37:57 +0600

    EasyDinner: Implemented global error handling

---
+++ EasyDinner/EasyDinner.Api/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,33 @@
+using System.Net;
+using System.Text.Json;
+
+namespace EasyDinner.Api.Midleware;
+public class ErrorHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    public ErrorHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        var code = HttpStatusCode.InternalServerError;
+        var result = JsonSerializer.Serialize(new { error = "An error occured when processing your request." });
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)code;
+        return context.Response.WriteAsync(result);
+    }
+}
--- EasyDinner/EasyDinner.Api/Program.cs
+++ EasyDinner/EasyDinner.Api/Program.cs
@@ -1,16 +1,24 @@
 
+using EasyDinner.Api.Errors;
+using EasyDinner.Api.Filters;
 using EasyDinner.Application;
 using EasyDinner.Infrastrcuture;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddApplication()
                     .AddInfrastrcuture(builder.Configuration);
+    // builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
     builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, EasyDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
+    // app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
