67a3c50123e2fd8ed9a0f455773f11c8976b2b67
Mohammad Arfizur Rahman <arahman@numberskills.se>
2022-12-26 11:40:26 +0600

    EasyDinner: Using OneOf to handle flow control

--- EasyDinner/EasyDinner.Application/Common/Interfaces/Errors/DuplicateEmailError.cs
+++ EasyDinner/EasyDinner.Application/Common/Interfaces/Errors/DuplicateEmailError.cs
@@ -1,3 +1,10 @@
+using System.Net;
+
 namespace EasyDinner.Application.Common.Errors;
 
-public record struct DuplicateEmailError();
+public record struct DuplicateEmailError : IError
+{
+    public HttpStatusCode StatusCode => HttpStatusCode.Conflict;
+
+    public string ErrorMessage => "Email already exists.";
+}
